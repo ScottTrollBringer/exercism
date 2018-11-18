93d6bce2c28246eebee523d663b4941c3fda7cb5
ScottTrollBringer <ian.hc.fraser@gmail.com>
2018-11-18 13:02:31 +0100

    Tentative de mettre leap en purement fonctionnel

--- leap.js
+++ leap.js
@@ -1,19 +1,8 @@
 // Ian FRASER - November 2018
 // Obj: Given a year, report if it is a leap year.
 export const isLeap = (year) => {
-  let biss = false;
-
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        biss = true;
-      } else {
-        biss = false;
-      }
-    } else {
-      biss = true;
-    }
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
   }
-
-  return biss;
+  return false;
 };
